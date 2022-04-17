bf309c4b760e191022527f00108842bbe23437d9
Ensar GÜNAYDIN <ensar@alisangroup.com>
2022-04-18 01:19:25 +0300

    First Commit

---
+++ app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import morgan from 'morgan';
+import cors from 'cors';
+import fileUpload from 'express-fileupload';
+import history from 'connect-history-api-fallback';
+import path from 'path';
+
+const app = express();
+
+// ! Middlewares
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(fileUpload({useTempFiles:true}));
+app.use(history());
+app.use(express.static(path.join(__dirname,'public')));
+
+// ! Routes
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+})
+
+// ! Settings
+app.set('port',process.env.PORT || 3000);
+
+app.listen(app.get('port'),"localhost",()=>{
+    console.log('Server on port '+app.get('port'));
+});

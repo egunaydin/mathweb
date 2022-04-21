1ae36fe731f10f4f2c06eecddba71cd5571731ec
Ensar GÜNAYDIN <ensar@alisangroup.com>
2022-04-21 23:48:12 +0300

    cloudinary added to project

--- controllers/professor.js
+++ controllers/professor.js
@@ -1,6 +1,9 @@
 import pool from "../database/keys";
+import cloudinary from "../lib/cloudinary";
 
 const professor = {};
+
+// ! Courses
 
 professor.createCourse = async (req, res) => {
   const { id, c_name, c_description } = req.body;
@@ -52,4 +55,59 @@ professor.updateCourse = async (req, res) => {
     });
   }
 };
+
+professor.deleteCourse = async (req, res) => {
+  const id = req.params.id_c;
+  try {
+    await pool.query("DELETE FROM course WHERE id_c = $1", [id]);
+    res.status(200).json({
+      message: "Successfully deleted course",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "An error has occurred",
+      error,
+    });
+  }
+};
+
+professor.getCourses = async (req, res) => {
+  const { id } = req.body;
+  try {
+    const courses = await (
+      await pool.query("SELECT * FROM course WHERE p_id=$1", [id])
+    ).rows;
+    res.status(200).json(courses);
+  } catch (error) {
+    res.status(500).json({
+      message: "An error has occurred",
+      error,
+    });
+  }
+};
+
+// ! Assignments
+
+professor.createAssignment = async (req, res) => {
+  const id_c = req.params.id_c;
+  const { a_name, a_description } = req.body;
+  const file = await cloudinary(req.files.a_file.tempFilePath);
+  console.log(req.files.a_file);
+  try {
+    await pool.query(
+      "INSERT INTO assignment(c_id , a_name , a_description , a_file) VALUES($1,$2,$3,$4)",
+      [id_c, a_name, a_description, file]
+    );
+    res.status(200).json({
+      message: "Succesfully added assignment",
+      assignment: { a_name, a_description, file },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "An error has occured",
+      error,
+    });
+  }
+};
+
 module.exports = professor;
--- routes/professor.routes.js
+++ routes/professor.routes.js
@@ -3,10 +3,22 @@ import professor from "../controllers/professor";
 
 const router = express.Router();
 
+// ! COURSES
+
 router.post("/course", professor.createCourse);
 
 router.get("/course/:id_c", professor.readCourse);
 
 router.put("/course/:id_c", professor.updateCourse);
+
+router.delete("/course/:id_c", professor.deleteCourse);
+
+router.get("/my-courses",professor.getCourses);
+
+// ! ASSIGNMENTS
+
+router.post('/assignment/:id_c',professor.createAssignment);
+
+// ! DELIVERIES
 
 module.exports = router;
